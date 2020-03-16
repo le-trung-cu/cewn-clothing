c2a6c41465986398fdc121c4af27ca0e02d99dec
le-trung-cu <trungcuqn@gmail.com>
2020-03-16 12:10:06 +0700

    created project with create react app node-sass and added files for homepage

---
+++ src/homepage.component.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div className='homepage'>
+            <div className="directory-menu">
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">hats</h1>
+                        <span className="subtitle">Shop now</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">jackets</h1>
+                        <span className="subtitle">Shop now</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">Sneakers</h1>
+                        <span className="subtitle">Shop now</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">womens</h1>
+                        <span className="subtitle">Shop now</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">Mens</h1>
+                        <span className="subtitle">Shop now</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
